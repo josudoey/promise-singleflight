c83c27f503367d4ac92a26eee0ff0ebc655b1348
Joey Fang <josudoey@gmail.com>
2022-05-17 01:41:34 +0800

    ci: modify for tsconfig

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.create = void 0;
-var createPromiseSingleflight_1 = require("./createPromiseSingleflight");
+exports.create = exports.createPromiseSingleflight = void 0;
+const createPromiseSingleflight_1 = require("./createPromiseSingleflight");
+Object.defineProperty(exports, "createPromiseSingleflight", { enumerable: true, get: function () { return createPromiseSingleflight_1.createPromiseSingleflight; } });
 Object.defineProperty(exports, "create", { enumerable: true, get: function () { return createPromiseSingleflight_1.createPromiseSingleflight; } });
 exports.default = createPromiseSingleflight_1.createPromiseSingleflight;
