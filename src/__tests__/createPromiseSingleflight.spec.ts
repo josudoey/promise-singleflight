ab266074c0d15c3dd1dec88b157289ac36db132b
Joey Fang <josudoey@gmail.com>
2022-05-28 21:51:57 +0800

    test: update for test case

--- src/__tests__/createPromiseSingleflight.spec.ts
+++ src/__tests__/createPromiseSingleflight.spec.ts
@@ -1,42 +1,39 @@
 import { createPromiseSingleflight } from '../createPromiseSingleflight'
+let singleflight = createPromiseSingleflight<string, string>()
+
+beforeEach(function () {
+  singleflight = createPromiseSingleflight <string, string>()
+})
 
 describe('createPromiseSingleflight', function () {
-  let singleflight = createPromiseSingleflight <string, string>()
-
-  beforeEach(function () {
-    singleflight = createPromiseSingleflight <string, string>()
-  })
-
-  describe('async fn', function () {
-    let count = 0
-    const delay = async function (ms: number): Promise<undefined> {
-      return await new Promise(function (resolve) {
-        setTimeout(resolve, ms)
-      })
-    }
-
-    const incrFn = async function (ms: number): Promise<string> {
-      const result = count.toString()
-      count++
-      await delay(ms)
-      return result
-    }
-
-    const queryForIncr = async function (key: string, ms: number): Promise<string> {
-      return await singleflight(key, async function (): Promise<string> {
-        return await incrFn(ms)
-      })
-    }
-
-    it('queryForIncr', async function () {
-      const items = await Promise.all([
-        queryForIncr('same', 10),
-        queryForIncr('same', 10),
-        queryForIncr('diff', 10)
-      ])
-      expect(items[0]).toBe('0')
-      expect(items[1]).toBe('0')
-      expect(items[2]).toBe('1')
+  let count = 0
+  async function delay (ms: number): Promise<void> {
+    return await new Promise<void>(function (resolve) {
+      setTimeout(resolve, ms)
     })
+  }
+
+  async function incrFn (ms: number): Promise<string> {
+    const result = count.toString()
+    count++
+    await delay(ms)
+    return result
+  }
+
+  const queryForIncr = async function (key: string, ms: number): Promise<string> {
+    return await singleflight(key, async function (): Promise<string> {
+      return await incrFn(ms)
+    })
+  }
+
+  it('queryForIncr', async function () {
+    const items = await Promise.all([
+      queryForIncr('same', 10),
+      queryForIncr('same', 10),
+      queryForIncr('diff', 10)
+    ])
+    expect(items[0]).toBe('0')
+    expect(items[1]).toBe('0')
+    expect(items[2]).toBe('1')
   })
 })
